258d78b26c23f961b24ea08a34eb500f3329db1b
muttsii <mats.jonsson.linkoping@gmail.com>
2022-11-12 20:17:30 +0100

    Moved logic to application, using MediatR to get response from handler to client

--- API/Startup.cs
+++ API/Startup.cs
@@ -11,7 +11,8 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Persistence;
-
+using MediatR;
+using Application.Activities;
 
 namespace API
 {
@@ -44,6 +45,7 @@ namespace API
                     policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
                 });
             });
+            services.AddMediatR(typeof(List.Handler).Assembly);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
